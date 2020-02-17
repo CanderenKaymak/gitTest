9ae7dcea57766f35bc74df64618076e07b30e371
Canderen Kaymak <canderenkaymak@gmail.com>
2020-02-17 15:51:33 +0300

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Canderen Kaymak on 17.02.2020.
+//  Copyright © 2020 Canderen Kaymak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
